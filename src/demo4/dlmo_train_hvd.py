142d762a6ce6a39ab0ec64494137f6276e5e909c
prabhatkc <prabhat.kc077@gmail.com>
2026-04-16 13:50:29 -0400

    correcting issues related to metric counters and seperating train and validation subroutines

--- src/demo4/dlmo_train_hvd.py
+++ src/demo4/dlmo_train_hvd.py
@@ -12,8 +12,7 @@
 #
 # To use, source the following environment.
 # source /anaconda3/base_env.sh
-# source /anaconda3/horovod_sm80_env.sh
-
+# conda activate dlmo # (with horovod build)
 import torch
 import numpy as np
 import argparse
@@ -52,9 +51,8 @@ parser.add_argument('--pretrained-model-epoch', type=int, default=170, help='Tra
 parser.add_argument('--nepochs', type=int, default=50, help='number of epochs to train')
 parser.add_argument('--batch-size', help='Training batch size.', type=int)
 parser.add_argument('--val-batch-size', type=int, default=16, help='batch size for validation/tuning data.')
-parser.add_argument('--batches-per-allreduce', type=int, default=1,
-                    help='number of batches processed locally before executing allreduce across workers;'
-                    'It multiplies the total batch size. (RHR: 1 loss function eqs 1 batches-per-allreduce')
+parser.add_argument('--wd', type=float, default=0.0,
+                    help='weight decay a.k.a regularization on weights')
 parser.add_argument('--shuffle_patches', action="store_true", \
                     help="shuffles the train/validation patch pairs(input-target) at \
                     utils.data.DataLoader & not at the HDF5dataloader")
@@ -73,13 +71,12 @@ if acceleration not in [1, 2, 4, 6, 8]:
     print('Invalid acceleration factor.', flush=True)
     sys.exit()
 # CMD variables ---------------------------------------------
-batch_size             = args.batch_size #training batch size 
+batch_size             = args.batch_size # training batch size 
 val_batch_size         = args.val_batch_size # validation batch size 
-batches_per_allreduce  = args.batches_per_allreduce
+batches_per_allreduce  = 1 # to implement other values, refer to https://github.com/prabhatkc/ct-recon/blob/main/Denoising/DLdenoise/main_hvd.py#L275
 allreduce_batch_size   = batch_size * batches_per_allreduce
 max_epochs             = args.nepochs
 epoch_start            = 0
-verbose_batch_idx      = int(val_batch_size/4)
 
 # Paths to i/o files --------------------------------------
 train_data_path        = args.train_data_path   
@@ -89,10 +86,8 @@ output_path            = args.output_path
 pretrained_model_path  = args.pretrained_model_path
 pretrained_model_epoch = args.pretrained_model_epoch
 
-dim1, dim2        = 260, 311
-cmpr_dtype        = 'float32'
-
-if cmpr_dtype == 'float16':
+cmpr_dtype = 'float32'
+if cmpr_dtype   == 'float16':
     torch_dtype = torch.float16
 else:
     torch_dtype = torch.float32
@@ -147,32 +142,23 @@ train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset, \
 train_loader  = torch.utils.data.DataLoader(train_dataset, batch_size=allreduce_batch_size, \
                sampler=train_sampler, **kwargs)
 tr_tot_size   = train_dataset.data.shape[0]
-#dim1          = train_dataset.data.shape[2]
-#dim2          = train_dataset.data.shape[3]
+dim1          = train_dataset.data.shape[2]
+dim2          = train_dataset.data.shape[3]
 
-# ensuring total size % (batch-size*NPUG) =0 is taken care withing hdf5 read function ------
-#if tr_tot_size % (batch_size*hvd.size()) != 0:
-#    print("ERROR! total training images must be divisible by train batch size * NGPUS.")
-#    sys.exit("ERROR! re-enter a differnt training batch size.")
-# ------------------------------------------------------------------------------------------
 if hvd.rank() == 0: print(" Dimension of training (input <-> target) batches is: {} <-> {}"\
                         .format(train_dataset.data.shape, train_dataset.target.shape))
     
 # ==================================================================
 # load validation data
 # ==================================================================
-val_dataset = DatasetFromHdf5(hvd=hvd, file_path=val_data_path, \
+val_dataset   = DatasetFromHdf5(hvd=hvd, file_path=val_data_path, \
                               mod_num=hvd.size() * val_batch_size)
-val_sampler = torch.utils.data.distributed.DistributedSampler(
-    val_dataset, num_replicas=hvd.size(), rank=hvd.rank(), shuffle=args.shuffle_patches)
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=val_batch_size,
+val_sampler   = torch.utils.data.distributed.DistributedSampler(
+                val_dataset, num_replicas=hvd.size(), rank=hvd.rank(), shuffle=args.shuffle_patches)
+val_loader    = torch.utils.data.DataLoader(val_dataset, batch_size=val_batch_size,
                                          sampler=val_sampler, **kwargs)
-val_tot_size = val_dataset.data.shape[0]
-# ensuring total size % (batch-size*NPUG) =0 is taken care withing hdf5 read function ------
-# if val_tot_size % (val_batch_size*hvd.size()) != 0:
-#    print("ERROR! total validation images must be divisible by val batch size * NGPUS.")
-#    sys.exit("ERROR! re-enter a differnt validation batch size.")
-# ------------------------------------------------------------------------------------------
+val_tot_size  = val_dataset.data.shape[0]
+
 if hvd.rank() == 0: print(" Dimension of validation (input <-> target) batches is: {} <-> {}"\
                             .format(val_dataset.data.shape, val_dataset.target.shape))
 # ==================================================================
@@ -184,9 +170,9 @@ net = Net(dim1=dim1, dim2=dim2, filter_size=7)
 # transfer models to cuda
 if use_cuda: model = net.cuda()
 
-optimizer = optim.Adam(model.parameters(), \
-                       lr=(1e-5 * batches_per_allreduce * hvd.size()), \
-                       weight_decay=args.wd)
+optimizer   = optim.Adam(model.parameters(), \
+              lr=(1e-5 * batches_per_allreduce * hvd.size()), \
+              weight_decay=args.wd)
 
 # Horovod: (optional) compression algorithm.
 compression = hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.none
@@ -216,69 +202,72 @@ hvd.broadcast_optimizer_state(optimizer, root_rank=0)
 # train the model
 # ==================================================================
 if hvd.rank() == 0: print("Training .... ")
-train_loss = utils.Metric('train_loss')
-val_loss = utils.Metric('val_loss')
-train_acc_his = np.empty((max_epochs, 1))
-val_acc_his = np.empty((max_epochs, 1))
-train_loss_his = np.empty((max_epochs, 1))
-val_loss_his = np.empty((max_epochs, 1))
-for epoch in range(epoch_start, max_epochs):
-    # Training
+
+train_acc_arr = np.empty((max_epochs, 1))
+val_acc_arr   = np.empty((max_epochs, 1))
+train_loss_arr= np.empty((max_epochs, 1))
+val_loss_arr  = np.empty((max_epochs, 1))
+
+def train(epoch):
+    model.train()
+    train_loss = utils.Metric('train_loss')
+    train_acc  = utils.Metric('train_acc')
     with tqdm(total=len(train_loader),
             desc='Epoch  #{}'.format(epoch + 1),
             disable=not verbose) as t:
         for batch_idx, (data, target) in enumerate(train_loader):
-
-            if use_cuda: data, target = data.cuda(), target.cuda()
-
-            model.train()
+            if use_cuda: 
+                data, target = data.cuda(), target.cuda()
             optimizer.zero_grad()
-            output = model(data)
-            loss = criterion(output, target)
+            output  = model(data)
+            loss    = criterion(output, target)
             loss.backward()
             train_loss.update(loss, hvd)
-            
-            #if hvd.rank()==0: 
-            #    print ('target', target.shape, target.dtype, '\n', target.cpu().detach().numpy() )
-            #    print ('output', output.shape, output.dtype, '\n', output.cpu().detach().numpy())
-            
-            cur_train_acc = roc_auc_score(np.squeeze(target.cpu().detach().numpy()), np.squeeze(output.cpu().detach().numpy()))
+            roc_acc = torch.tensor(roc_auc_score(np.squeeze(target.cpu().detach().numpy()), np.squeeze(output.cpu().detach().numpy())))
+            train_acc.update(roc_acc, hvd)
             optimizer.step()
 
-            # Validation step (every val_batch_size/4)
-            if (batch_idx + 1) % (verbose_batch_idx) == 0:
-                with torch.no_grad():
-                    val_outputs_flag = 0
-                    for data, target in val_loader:
-                        if use_cuda:
-                            data, target = data.cuda(), target.cuda()
+            t.set_postfix({'train acc': train_acc.avg.item(), 'train loss': train_loss.avg.item()})
+            t.update(1)
+    if args.save_log_ckpts:
+        train_acc_arr[epoch]  = train_acc.avg.item()
+        train_loss_arr[epoch] = train_loss.avg.item()
 
-                        model.eval()
-                        output = model(data)
-                        if val_outputs_flag == 0:
-                            val_outputs = output
-                            val_targets = target
-                            val_outputs_flag = 1
-                        elif val_outputs_flag == 1:
-                            val_outputs = torch.cat((val_outputs, output), 0)
-                            val_targets = torch.cat((val_targets, target), 0)
-                        loss = criterion(output, target)
-                        val_loss.update(loss, hvd)
-                cur_val_acc = roc_auc_score(val_targets.cpu().numpy(), val_outputs.cpu().numpy())
-                t.set_postfix({'train acc': cur_train_acc, \
-                               'val acc': cur_val_acc, \
-                               'train loss': train_loss.avg.item()})
-                t.update(verbose_batch_idx)
+def validate(epoch):
+    model.eval()
+    val_loss = utils.Metric('val_loss')
+    val_acc  = utils.Metric('val_acc')
+    with tqdm(total=len(val_loader),
+            desc='Epoch  #{}'.format(epoch + 1),
+            disable=not verbose) as t:
+        with torch.no_grad():
+            for batch_idx, (data, target) in enumerate(val_loader):
+                if use_cuda: 
+                    data, target = data.cuda(), target.cuda()
+                output  = model(data)
+                loss    = criterion(output, target)
+                val_loss.update(loss, hvd)
+                roc_acc = torch.tensor(roc_auc_score(np.squeeze(target.cpu().detach().numpy()), np.squeeze(output.cpu().detach().numpy())))
+                val_acc.update(roc_acc, hvd)
+                t.set_postfix({'val acc': val_acc.avg.item(), 'val loss': val_loss.avg.item()})
+                t.update(1)
+    if args.save_log_ckpts:
+        val_acc_arr[epoch]  = val_acc.avg.item()
+        val_loss_arr[epoch] = val_loss.avg.item()
 
-    # Save checkpoints and logs if enabled
-    if (args.save_log_ckpts):
-        utils.save_checkpoint(epoch, args, hvd, model, optimizer)
-        train_acc_his[epoch]  = cur_train_acc
-        val_acc_his[epoch]    = cur_val_acc
-        train_loss_his[epoch] = train_loss.avg.item()
-        val_loss_his[epoch]   = val_loss.avg.item()
-        loss_acc = {'train acc': train_acc_his[train_acc_his != 0], \
-                    'val acc': val_acc_his[val_acc_his != 0], \
-                    'train loss': train_loss_his[train_loss_his != 0], \
-                    'val loss': val_loss_his[val_loss_his != 0]}
-        utils.save_log_to_h5(args, hvd, loss_acc)
+# ====================================================================
+# Main function with train, validate; save weights and metrics
+# ======================================================================
+for epoch in range(epoch_start, max_epochs):
+    train(epoch)
+    validate(epoch)
+    # save trained weights 
+    if (args.save_log_ckpts): utils.save_checkpoint(epoch, args, hvd.rank(), model, optimizer)
+
+# saving per epoch metrics into an h5 file 
+if (args.save_log_ckpts):
+    loss_acc = {'train acc' : train_acc_arr[train_acc_arr != 0], \
+                'val acc'   : val_acc_arr[val_acc_arr != 0], \
+                'train loss': train_loss_arr[train_loss_arr != 0], \
+                'val loss'  : val_loss_arr[val_loss_arr != 0]}
+    utils.save_log_to_h5(args, hvd.rank(), loss_acc)
